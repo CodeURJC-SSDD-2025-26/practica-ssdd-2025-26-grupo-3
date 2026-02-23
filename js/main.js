0ec6e4ae0bbd5070348e671976178f750faf30b2
Demelza <demelzasantamariagil@gmail.com>
2026-02-24 00:30:09 +0100

    Refactored code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-/*All images, both user icon and application logo, will redirect to their corresponding pages */
-document.getElementById("logo").addEventListener("click", function () {
-  window.location.href = "home.html";
-});
+document.addEventListener("DOMContentLoaded", function () {
+  /*Sign-in and Sign-up buttons*/
+  document
+    .getElementById("create-account-button")
+    .addEventListener("click", function () {
+      window.location.href = "sign_up.html";
+    });
 
-document.getElementById("user").addEventListener("click", function () {
-  window.location.href = "user_profile.html";
-});
+  document
+    .getElementById("sign-in-button")
+    .addEventListener("click", function () {
+      window.location.href = "sign_in.html";
+    });
 
-/*Sign-in and Sign-up buttons*/
-document
-  .getElementById("create-account-button")
-  .addEventListener("click", function () {
-    window.location.href = "sign_up.html";
+  /*All images, both user icon and application logo, will redirect to their corresponding pages */
+  document.getElementById("logo").addEventListener("click", function () {
+    window.location.href = "home.html";
   });
 
-document
-  .getElementById("sign-in-button")
-  .addEventListener("click", function () {
-    window.location.href = "sign_in.html";
+  document.getElementById("user").addEventListener("click", function () {
+    window.location.href = "user_profile.html";
   });
+});
