fc632b9cae0f4a1cab5e5e36b10a4ff0774c3dd7
Demelza <demelzasantamariagil@gmail.com>
2026-02-23 20:48:56 +0100

    Added histogram, code refactored and added some functionalities

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ document.getElementById("logo").addEventListener("click", function () {
 document.getElementById("user").addEventListener("click", function () {
   window.location.href = "user_profile.html";
 });
+
+/*Sign-in and Sign-up buttons*/
+document
+  .getElementById("create-account-button")
+  .addEventListener("click", function () {
+    window.location.href = "sign_up.html";
+  });
+
+document
+  .getElementById("sign-in-button")
+  .addEventListener("click", function () {
+    window.location.href = "sign_in.html";
+  });
