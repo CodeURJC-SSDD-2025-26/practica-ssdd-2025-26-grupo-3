ad1a0016427b98845324ca2732c94a5f97e3469a
Demelza <demelzasantamariagil@gmail.com>
2026-03-18 18:11:47 +0100

    Created Spring Boot Proyect

---
+++ postcredits/src/main/java/es/codeurjc/postcredits/PostcreditsApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.postcredits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcreditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcreditsApplication.class, args);
+	}
+
+}
