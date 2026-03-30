ac89ec171c91c9877824d32163d706db4e924217
SergioMunozLaureiro <m.l.sergio77@gmail.com>
2026-03-30 20:02:03 +0200

    Modified home controller adding ListService attributes, & home.html using Mustache on product cards

--- postcredits/src/main/java/es/codeurjc/postcredits/HomeController.java
+++ postcredits/src/main/java/es/codeurjc/postcredits/HomeController.java
@@ -1,11 +1,13 @@
 package es.codeurjc.postcredits;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired ListService listService;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -15,6 +17,8 @@ public class HomeController {
         model.addAttribute("sign_up",true);
         model.addAttribute("search_bar", true);
         model.addAttribute("user", true);
+        model.addAttribute("allProducts", listService.getProducts());
+       
 
         return "home";
     }
